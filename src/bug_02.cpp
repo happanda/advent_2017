5d264eb04179fd13f7eaee06f07f53cd186180a9
Pavel Moskalevich <happy.panda.code@gmail.com>
2017-12-04 21:30:39 +0300

    solution for bug 2

--- src/bug_02.cpp
+++ src/bug_02.cpp
@@ -1,25 +1,88 @@
+#include <algorithm>
 #include <iostream>
+#include <limits>
+#include <sstream>
+#include <vector>
 #include "advent.h"
 
+
+const char* input_01 = R"(626	2424	2593	139	2136	163	1689	367	2235	125	2365	924	135	2583	1425	2502
+183	149	3794	5221	5520	162	5430	4395	2466	1888	3999	3595	195	181	6188	4863
+163	195	512	309	102	175	343	134	401	372	368	321	350	354	183	490
+2441	228	250	2710	200	1166	231	2772	1473	2898	2528	2719	1736	249	1796	903
+3999	820	3277	3322	2997	1219	1014	170	179	2413	183	3759	3585	2136	3700	188
+132	108	262	203	228	104	205	126	69	208	235	311	313	258	110	117
+963	1112	1106	50	186	45	154	60	1288	1150	986	232	872	433	48	319
+111	1459	98	1624	2234	2528	93	1182	97	583	2813	3139	1792	1512	1326	3227
+371	374	459	83	407	460	59	40	42	90	74	163	494	250	488	444
+1405	2497	2079	2350	747	1792	2412	2615	89	2332	1363	102	81	2346	122	1356
+1496	2782	2257	2258	961	214	219	2998	400	230	2676	3003	2955	254	2250	2707
+694	669	951	455	2752	216	1576	3336	251	236	222	2967	3131	3456	1586	1509
+170	2453	1707	2017	2230	157	2798	225	1891	945	943	2746	186	206	2678	2156
+3632	3786	125	2650	1765	1129	3675	3445	1812	3206	99	105	1922	112	1136	3242
+6070	6670	1885	1994	178	230	5857	241	253	5972	7219	252	806	6116	4425	3944
+2257	155	734	228	204	2180	175	2277	180	2275	2239	2331	2278	1763	112	2054)";
 
 template <>
 void runFix<1, 0>(int argc, char* argv[])
 {
-	const char* input = "5 1 9 5\
-		7 5 3\
-		2 4 6 8";
-	const int length = std::strlen(input);
-	int repeatsSum = 0;
-	for (int i = 0; i <= length; ++i)
+	int totalSum = 0;
+
+	std::istringstream iss(input_01);
+	std::string line;
+	while (std::getline(iss, line, '\n'))
 	{
-		if (input[i] == input[(i + 1) % length])
-			repeatsSum += input[i] - '0';
+		std::istringstream numTokens(line);
+		int number = 0,
+			min = std::numeric_limits<int>::max(),
+			max = 0;
+		while (numTokens >> number)
+		{
+			max = std::max(max, number);
+			min = std::min(min, number);
+		}
+		totalSum += max - min;
 	}
-	std::cout << repeatsSum << std::endl;
+
+	std::cout << totalSum << std::endl;
 }
 
 template <>
 void runFix<1, 1>(int argc, char* argv[])
 {
+	int totalSum = 0;
+
+	std::istringstream iss(input_01);
+	std::string line;
+
+	while (std::getline(iss, line, '\n'))
+	{
+		std::istringstream numTokens(line);
+
+		int number = 0;
+		std::vector<int> numbers;
+
+		while (numTokens >> number)
+			numbers.push_back(number);
+
+		[&] { // using anonymous lambda for break from inner loop
+			for (int i = 0; i < numbers.size() - 1; ++i)
+			{
+				for (int j = i + 1; j < numbers.size(); ++j)
+				{
+					const int min = std::min(numbers[i], numbers[j]);
+					const int max = std::max(numbers[i], numbers[j]);
+
+					if (max % min == 0)
+					{
+						totalSum += max / min;
+						return; // break out of both loops
+					}
+				}
+			}
+		}();
+	}
+
+	std::cout << totalSum << std::endl;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[])
 {
-	runFix<0, 0>(argc, argv);
+	runFix<1, 1>(argc, argv);
 	int pauseVar = 0;
 	std::cin >> pauseVar;
 	return 0;
