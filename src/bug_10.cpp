550de157d3c9ae752c6effc9d0b7420a7c7a1efa
Pavel Moskalevich <happy.panda.code@gmail.com>
2017-12-26 01:24:28 +0300

    solution for bug 10.1

--- src/bug_10.cpp
+++ src/bug_10.cpp
@@ -6,7 +6,7 @@
 #include "advent.h"
 
 
-std::string const input_08 = std::string(R"(206, 63, 255, 131, 65, 80, 238, 157, 254, 24, 133, 2, 16, 0, 1, 3)");
+std::string const input_08 = std::string(R"(206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3)");
 
 void reverse(std::vector<int>& vect, int from, int length)
 {
@@ -41,4 +41,60 @@ void runFix<10, 0>(int argc, char* argv[])
 	}
 
 	std::cout << sequence[0] * sequence[1] << std::endl;
+}
+
+template <>
+void runFix<10, 1>(int argc, char* argv[])
+{
+	int const numRounds = 64;
+	int const seqLength = 256;
+	int const xorLength = 16;
+
+	std::vector<int> sequence;
+	std::generate_n(std::back_inserter(sequence), seqLength, [&sequence]() {return sequence.size(); });
+
+	std::string input(input_08);
+	std::vector<int> lengths;
+	std::transform(input.begin(), input.end(), std::back_inserter(lengths), [](char symbol) { return static_cast<int>(symbol); });
+	for (int n : { 17, 31, 73, 47, 23 })
+		lengths.push_back(n);
+
+	int curInd = 0;
+	int skipSize = 0;
+	
+	for (int r = 0; r < numRounds; ++r)
+	{
+		for (int length : lengths)
+		{
+			if (length > static_cast<int>(sequence.size()))
+				continue;
+			reverse(sequence, curInd, length);
+
+			curInd += (length + skipSize) % seqLength;
+			skipSize = (skipSize + 1) % seqLength;
+		}
+	}
+
+	std::string xored(2 * seqLength / xorLength + 1, '\0');
+	for (int i = 0; i < seqLength / xorLength; ++i)
+	{
+		int xorResult = 0;
+		for (int j = 0; j < xorLength; ++j)
+		{
+			xorResult ^= sequence[i * xorLength + j];
+		}
+
+		char hex1 = (xorResult / 16) % 16;
+		char hex0 = xorResult % 16;
+		if (hex1 < 10)
+			xored[i * 2] = hex1 + '0';
+		else
+			xored[i * 2] = hex1 - 10 + 'a';
+		if (hex0 < 10)
+			xored[i * 2 + 1] = hex0 + '0';
+		else
+			xored[i * 2 + 1] = hex0 - 10 + 'a';
+	}
+
+	std::cout << xored << std::endl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,6 @@
 
 int main(int argc, char* argv[])
 {
-	runFix<10, 0>(argc, argv);
+	runFix<10, 1>(argc, argv);
 	return 0;
 }
